ce64cb3217aabe3a0121a306794a01ab17d03b7d
hisreal123 <omosgoerge@gmail.com>
2023-01-26 23:08:48 +0100

    ⚡️ working on AboutPage

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function page() {
+export default function page() {       
     return (
         <div className='bg-[#f7f7f7]'>
             <p className=' text-xl font-bold'> About page </p> </div>
